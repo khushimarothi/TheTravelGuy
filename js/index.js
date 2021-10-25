97dc74b45a416799ab23191b1ded6ae77dc0ceec
Khushi <khushimarothi2002@gmail.com>
2021-10-25 22:49:25 +0530

    commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+var loader = document.getElementById("preloader");
+window.addEventListener(
+  "load", function () {
+  loader.style.display = "none";
+});
+
 const colors=document.getElementsByClassName('theme-button');
 
 let i;
@@ -9,62 +15,151 @@ function changecolor(){
 
   let color = this.getAttribute('data-color');
   document.documentElement.style.setProperty('--dark', color);
-  
 
   let chng = this.getAttribute('data-color-light');
   document.documentElement.style.setProperty('--light', chng);
-
-
-/*  let sucgreen = this.getAttribute('data-color-success');
-  document.documentElement.style.setProperty('--success', sucgreen);
-  document.getElementById('succ').value= sucgreen;
-
-  
-  let warnyel = this.getAttribute('data-color-warning');
-  document.documentElement.style.setProperty('--warning', warnyel);
-  document.getElementById('warn').value= warnyel;*/
 }
 
+let btn1 = document.querySelector('#btn-1');
+let btn2 = document.querySelector('#btn-2');
 
 
-/*------------------------------*/
-
-let colorInput =document.querySelector('#color');
-let hexInput =document.querySelector('#hex');
-
-hexInput.addEventListener('click',()=>{
-    let clor = colorInput.value;
-    document.documentElement.style.setProperty('--color', clor);
-    
+btn1.addEventListener('click', () => {
+  var div = document.getElementById("change");
+  div.style.backgroundImage =" url('../images/trial.jpg')";
+  var news = document.getElementById("news");
+  news.style.backgroundImage =" url('../images/trial.jpg')";
+  var review = document.getElementById("review");
+  review.style.backgroundImage =" url('../images/trial.jpg')";
 });
 
-/*----------------------------------*/
-
-let secon =document.querySelector('#seco');
-let butIn =document.querySelector('#swatch');
-
-butIn.addEventListener('click',()=>{
-    let swa = secon.value;
-    document.documentElement.style.setProperty('--primary-color', swa);
+btn2.addEventListener('click', () => {
+  var div = document.getElementById("change");
+  div.style.backgroundImage =" url('../images/demobg2.jpg')";
+  var news = document.getElementById("news");
+  news.style.backgroundImage =" url('../images/demobg2.jpg')";
+  var review = document.getElementById("review");
+  review.style.backgroundImage =" url('../images/demobg2.jpg')";
 });
 
-/*--------------------------------------*/
+//validation--------------------------------------------->
 
-let succIn =document.querySelector('#succ');
-let In =document.querySelector('#swatch2');
+const form=document.getElementById('form');
+const uname=document.getElementById('name');
+const email=document.getElementById('email');
+const from=document.getElementById('from');
+const to=document.getElementById('to');
+const date = document.getElementById('sdate');
+const edate = document.getElementById('edate');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-In.addEventListener('click',()=>{
-    let green = succIn.value;
-    document.documentElement.style.setProperty('--success', green);
-});
+  checkInputs();
+  });
 
-/*---------------------------------------*/
+  function checkInputs(){
+ 
+    const nameValue = uname.value.trim();
+    const emailValue = email.value.trim();
+   const dateValue = date.value.trim();
+   const edateValue = edate.value.trim();
+   
+//name 
+  if( nameValue === ''){
+    setErrorFor(uname, 'Name cannot be blank');
+  } 
+  
+  else if(nameValue.length<=4){
+    setErrorFor(uname, 'Name minimum 5 character ');
+  }
+
+  else {
+   setSuccessFor(uname);
+  }
+//email  
+  if(emailValue === ''){
+    setErrorFor(email, 'Email cannot be blank');
+ }
+ else if (!isEmail(emailValue)) {
+   setErrorFor(email, 'Not a valid email');
+ } 
+ else{
+   setSuccessFor(email);
+ }
+ //date--------------------------------------------------
+ if( dateValue === '')
+{
+  setErrorFor(date, 'Date cannot be blank');
+}
+else{
+  setSuccessFor(date);
+}
+//end date-----------------------------------------------
+if( edateValue === '')
+{
+  setErrorFor(edate, 'Date cannot be blank');
+}
+else{
+  setSuccessFor(edate);
+}
+ //compare------------------------------------------------
+
+ if(edateValue < dateValue && date!='' )
+{
+  setErrorFor(edate, 'Enter valid Date');
+}
+//---------------------------------------------------------
+ 
 
 
-let warnIng =document.querySelector('#warn');
-let warn =document.querySelector('#swatch3');
+  } 
+  //start date
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+if(dd<10)
+{
+  dd='0'+ dd
+} 
+if(mm<10)
+{
+  mm='0'+mm
+}
+today=yyyy+'-'+mm+'-'+dd;
+document.getElementById("sdate").setAttribute("min" , today);
 
-warn.addEventListener('click',()=>{
-    let yellow = warnIng.value;
-    document.documentElement.style.setProperty('--warning', yellow);
-});
+//end date
+  
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+if(dd<10)
+{
+  dd='0'+ dd
+} 
+if(mm<10)
+{
+  mm='0'+mm
+}
+today=yyyy+'-'+mm+'-'+dd;
+document.getElementById("edate").setAttribute("min" , today);
+
+
+
+function setErrorFor(input, message){
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+   small.innerText = message;
+   formControl.className = 'col-md-6 error';
+}
+
+function setSuccessFor(input){
+  const formControl = input.parentElement;
+  formControl.className = 'col-md-6 success';
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
